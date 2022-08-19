9c64b5eb13dc699abc38e02399c4cceef7a5ab5b
DarkSparkNecron <110418221+DarkSparkNecron@users.noreply.github.com>
2022-08-19 23:59:48 +0300

    version up to date
    So while im breaking things why not to update natgen version as well to 0.8.0DEV

--- src/nationGen/NationGen.java
+++ src/nationGen/NationGen.java
@@ -34,8 +34,8 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class NationGen
 {
-	public static String version = "0.7.0-RC12";
-	public static String date = "14th of January 2021";
+	public static String version = "0.8.0DEV";
+	public static String date = "19th of August 2022";
 	
 	private List<NationRestriction> restrictions;
 	
